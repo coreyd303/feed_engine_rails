3eb9571b1f60a9422d7b5b775388c187c88d2198
Andy Mention <amention@gmail.com>
2014-10-30 11:36:02 -0600

    All three models are chillin'

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -8,16 +8,19 @@ class Api::V1::GroupsController < ApplicationController
 
   def create
     @group = Group.create(safe_params)
+    render :json => @trip
   end
 
   def update
     @group = Group.find(params[:id])
     @group.update(safe_params)
+    render :json => @trip
   end
 
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
+    head :ok
   end
 
   def safe_params 
