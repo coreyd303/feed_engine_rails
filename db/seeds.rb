dcae6f5f1735ac0533d217661bcea0beefdacbe8
Andy Mention <amention@gmail.com>
2014-11-05 22:58:23 -0700

    remove group controller, model, serializer, migration, groupuser migration, spec, factory, and seed method from app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,11 @@
 class Seed
   attr_reader :users_count, :groups_count, :trips_count
 
-  def initialize(users_count = 100, groups_count = 10, trips_count = 50)
+  def initialize(users_count = 100, trips_count = 50)
     @users_count = users_count
-    @groups_count = groups_count
     @trips_count = trips_count
     generate_users
-    generate_groups
     generate_trips
-    add_users_to_groups
-    add_trips_to_groups
   end
 
   def generate_users
@@ -22,15 +18,6 @@ class Seed
       puts "#{users_count} users created"
   end
 
-  def generate_groups
-    groups_count.times do |i|
-      group = Group.create(name: Faker::Company.name,
-                           description: Faker::Lorem.sentences.join
-                           )
-      end
-    puts "#{groups_count} Groups created"
-  end
-
   def generate_trips
     trips_count.times do |i|
       trip = Trip.create(name: Faker::Company.name,
@@ -40,23 +27,6 @@ class Seed
                          )
     end
     puts "#{trips_count} Trips created"
-  end
-
-  def add_users_to_groups
-    users = User.all
-    users << User.first(20)
-    users.each do |user|
-      group = Group.find(rand(1..Group.count))
-      group.users << user
-    end 
-  end
-
-  def add_trips_to_groups
-    trips = Trip.all
-    trips.each do |trip|
-      group = Group.find(rand(1..Group.count))
-      group.trips << trip
-    end
   end
 end
 
