2853621459e64f5a634344b44a5adf2ac004c6fb
Andy Mention <amention@gmail.com>
2014-11-11 09:47:35 -0700

    GetUserInstas works for trips as a background worker.

--- app/workers/get_user_instas.rb
+++ app/workers/get_user_instas.rb
@@ -1,5 +1,5 @@
 class GetUserInstas
-#  include Sidekiq::Worker
+  include Sidekiq::Worker
   attr_accessor :trip, :user
 
   def perform(user, trip)
@@ -7,20 +7,19 @@ class GetUserInstas
     @user = user
     feed = Instagram.get("https://api.instagram.com/v1/users/#{user.instagram_id}/media/recent", max_timestamp: max_time, min_timestamp: min_time)
     feed_url = "https://api.instagram.com/v1/users/#{user.instagram_id}/media/recent/?max_timestamp=#{max_time}&min_timestamp=#{min_time}"
-    binding.pry
     save_instas(feed, user, trip)
   end
 
   def save_instas(feed, user, trip)
     feed.each do |insta|
-    #  unless Insta.where("insta_url = #{insta['link']}")
+      unless Insta.where("insta_url = #{insta['link']}")
         Insta.create!(insta_id: insta['user']['id'].to_i,
                       user_id: user.id,
                       trip_id: trip.id,
                       insta_url: insta['link'],
                       thumbnail_url: insta['images']['thumbnail']['url'],
                       full_size_url: insta['images']['standard_resolution']['url'])
-     # end
+      end
     end
   end
 
